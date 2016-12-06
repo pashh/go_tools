d3edb70b4f31cc3235b21face3f989bb8ce7aeed
Kharskiy <Pavel.Kharskiy@external.telekom.de>
2016-12-06 21:23:09 +0300

    update dwh report sender

--- run.go
+++ run.go
@@ -1,13 +1,55 @@
 package main
 
-import "flag"
-import "fmt"
-import "os"
-import "regexp"
-import "os/exec"
-import "sync"
-import "strings"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"os/exec"
+	"sync"
+	"strings"
+	"bufio"
+	"log"
+	"reflect"
+	"math/rand"
+	"time"
+)
 //import "syscall"
+
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
+}
+
+
+
+
 
 
 func IsValidUUID(uuid string) bool {
@@ -32,27 +74,58 @@ func exe_cmd(cmd string, wg *sync.WaitGroup) {
 }
 
 
+func random(min, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return (rand.Intn(max - min) + min)
+}
+
 func main() {
 
 	voFlag := flag.String("VO", "", "VO for report")
 	uuidFlag := flag.String("UUID", "", "please set a valid uuid")
-
 	flag.Parse()
-
 	//_=voFlag
 	//_=uuidFlag
-
 	if *voFlag =="" || IsValidUUID(*uuidFlag)==false{
 		fmt.Println("one of mandatory options missing or wrong a sample execution: ./run -VO sdger -UUID 550e8400-e29b-41d4-a716-446655440000")
 		os.Exit(1)
 	}
 
+	var dwhp_report_name string= fmt.Sprintf("Reporting_%s_%s.csv", *voFlag, *uuidFlag)  
+	fmt.Println("the new report name: ", dwhp_report_name)
 
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	exe_cmd("ls -la", wg)
-	wg.Wait()
+		
+	//fmt.Println(flag.Args())
+
+
+
+	lines, err := readLines("./sample_dwhp_report/Reporting.csv")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	fmt.Println(reflect.TypeOf(lines))
+	fmt.Println(len(lines))
+
+	var line int = random(90921, 181842)
+
+
+//	for i := 1; i <= line; i++ {
+//		fmt.Println(i, lines[i])
+//	}
+
+	if err := writeLines(lines[:line], dwhp_report_name); err != nil {
+		log.Fatalf("writeLines: %s", err)
+	}
 	
-	fmt.Println(*uuidFlag)
-	fmt.Println(flag.Args())
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	//var cmd string=fmt.Sprintf("scp ./%s tvpp@10.1.234.189:/svc/tmv1684/moip/mo30501/dwhp", dwhp_report_name)
+	var cmd string=fmt.Sprintf("scp -P 6189 ./%s tvpp@localhost:/svc/tmv1684/moip/mo30501/dwhp", dwhp_report_name)
+	exe_cmd(cmd, wg)
+	
+	exe_cmd(fmt.Sprintf("rm -rf ./%s",dwhp_report_name), wg)
+	wg.Wait()
+
+
 }
